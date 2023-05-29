9727a13e4ac0c8e6c9e0511059f7147218fec3f1
Momo8289 <nicholashg8289@gmail.com>
2023-05-28 23:35:43 -0400

    actual code

---
+++ file_input.py
@@ -0,0 +1,92 @@
+# https://stackoverflow.com/questions/29991917/indices-of-matching-parentheses-in-python
+def find_parens(s):
+    toret = {}
+    pstack = []
+
+    for i, c in enumerate(s):
+        if c == '[':
+            pstack.append(i)
+        elif c == ']':
+            if len(pstack) == 0:
+                raise IndexError("No matching closing parens at: " + str(i))
+            toret[pstack.pop()] = i
+
+    if len(pstack) > 0:
+        raise IndexError("No matching opening parens at: " + str(pstack.pop()))
+
+    return toret
+
+
+EOF_SYMBOL = 0
+
+
+def run(raw, state=None):
+    # Get program input
+    def get_input():
+        with open("input", "r") as file:
+            for r in file.read():
+                yield r.strip("\n")
+
+    program_input = get_input()
+    # Clean input
+    raw = "".join([c for c in raw if c in "<>,.[]+-"])
+    if state is None:
+        state = {}
+    pointer = 0
+    index = 0
+    opened = find_parens(raw)
+    closed = {v: k for k, v in opened.items()}
+    output = []
+    while index < len(raw):
+        char = raw[index]
+        if char == ">":
+            pointer += 1
+
+        elif char == "<":
+            pointer -= 1
+
+        elif char == "+":
+            if not state.get(pointer):
+                state[pointer] = 0
+            state[pointer] = (state[pointer] + 1) % 256
+
+        elif char == "-":
+            if not state.get(pointer):
+                state[pointer] = 0
+            state[pointer] = (state[pointer] - 1) % 256
+            if state[pointer] == 0:
+                del state[pointer]
+
+        elif char == ".":
+            output.append(state.get(pointer) or 0)
+
+        elif char == ",":
+            try:
+                i = next(program_input)
+            except StopIteration:
+                i = EOF_SYMBOL
+            if i == "":
+                continue
+            try:
+                state[pointer] = ord(i)
+            except TypeError:
+                state[pointer] = int(i)
+        elif char == "[":
+            if (state.get(pointer) or 0) == 0:
+                index = opened[index]
+
+        elif char == "]":
+            if (state.get(pointer) or 0) != 0:
+                index = closed[index]
+
+        index += 1
+
+    return output, state
+
+
+with open("program.bf", "r") as file:
+    raw = file.read()
+
+output, state = run(raw)
+print("".join(chr(c) for c in output))
+print(state)
